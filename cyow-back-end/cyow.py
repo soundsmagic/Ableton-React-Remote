5e6711f15394cdfccf5b8244006a2ce4440d6a55
John Svensson <john-svensson@hotmail.com>
2022-02-15 17:49:56 +0100

    Sketching out back end structure

--- cyow-back-end/cyow.py
+++ cyow-back-end/cyow.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
+import imp
 from _Framework.ControlSurface import ControlSurface
+from .server import Server
+
 
 class Cyow(ControlSurface):
     def __init__(self):
         super(Cyow, self).__init__(c_instance)
-        self.
+        self.server = Server()
